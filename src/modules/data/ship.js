4b6f884c80c4d04c7e8f613a13d35c53f9071c3b
ngodi <albertngodi@gmail.com>
2019-11-08 01:24:45 +0100

    ship factory setup

---
+++ src/modules/data/ship.js
@@ -0,0 +1,9 @@
+const shipFactory = (len) => {
+  const hitPos = [];
+  const hit = (position) => hitPos.push(position);
+  const isSunk = () => hitPos.length >= len;
+
+return { len, isSunk, hit};
+};
+
+export default shipFactory
